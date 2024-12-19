8afbe23406b7cb268e2bea432c87ecb86d35d8cb
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-12-19 22:04:35 +0100

    Chore: implement settings to sidebar and improve sidebar view

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -17,6 +17,8 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
+import Link from "next/link";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 export function NavMain({
   items,
@@ -25,10 +27,14 @@ export function NavMain({
     title: string;
     url: string;
     icon?: LucideIcon;
+    imageUrl?: string;
     isActive?: boolean;
+    comingSoon?: boolean;
     items?: {
       title: string;
       url: string;
+      icon?: LucideIcon;
+      comingSoon?: boolean;
     }[];
   }[];
 }) {
@@ -38,29 +44,85 @@ export function NavMain({
       <SidebarMenu>
         {items.map((item) => (
           <Collapsible
-            key={item.title}
+            key={`NavMain-${item.title}`}
             asChild
             defaultOpen={item.isActive}
             className='group/collapsible'
           >
             <SidebarMenuItem>
               <CollapsibleTrigger asChild>
-                <SidebarMenuButton tooltip={item.title}>
-                  {item.icon && <item.icon />}
-                  <span>{item.title}</span>
-                  <ChevronRight className='ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90' />
-                </SidebarMenuButton>
+                {item.comingSoon ? (
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <div>
+                        <SidebarMenuSubButton className='opacity-70'>
+                          {item.icon && <item.icon />}
+                          <span>{item.title}</span>
+                        </SidebarMenuSubButton>
+                      </div>
+                    </TooltipTrigger>
+                    <TooltipContent side='right' sideOffset={8}>
+                      Coming soon
+                    </TooltipContent>
+                  </Tooltip>
+                ) : (
+                  <SidebarMenuButton
+                    tooltip={item.title}
+                    asChild={
+                      !item.items || item.items?.length === 0 ? true : false
+                    }
+                  >
+                    {!item.items || item.items?.length === 0 ? (
+                      <Link href={item.url}>
+                        {item.icon && <item.icon />}
+                        <span>{item.title}</span>
+                      </Link>
+                    ) : (
+                      <>
+                        {item.icon && <item.icon />}
+                        {item.imageUrl && (
+                          <img
+                            src={item.imageUrl}
+                            className='w-4 h-4 rounded aspect-square'
+                          />
+                        )}
+                        <span>{item.title}</span>
+                        <ChevronRight className='ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90' />
+                      </>
+                    )}
+                  </SidebarMenuButton>
+                )}
               </CollapsibleTrigger>
+
               <CollapsibleContent>
                 <SidebarMenuSub>
                   {item.items?.map((subItem) => (
-                    <SidebarMenuSubItem key={subItem.title}>
-                      <SidebarMenuSubButton asChild>
-                        <a href={subItem.url}>
-                          <span>{subItem.title}</span>
-                        </a>
-                      </SidebarMenuSubButton>
-                    </SidebarMenuSubItem>
+                    <div key={`SidebarMenuSub-${subItem.title}`}>
+                      {subItem.comingSoon ? (
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <div>
+                              <SidebarMenuSubButton className='opacity-70'>
+                                {subItem.icon && <subItem.icon />}
+                                <span>{subItem.title}</span>
+                              </SidebarMenuSubButton>
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent side='right' sideOffset={8}>
+                            Coming soon
+                          </TooltipContent>
+                        </Tooltip>
+                      ) : (
+                        <SidebarMenuSubItem>
+                          <SidebarMenuSubButton asChild>
+                            <Link href={subItem.url}>
+                              {subItem.icon && <subItem.icon />}
+                              <span>{subItem.title}</span>
+                            </Link>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      )}
+                    </div>
                   ))}
                 </SidebarMenuSub>
               </CollapsibleContent>
